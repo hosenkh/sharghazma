0a399142d66635c694e8f7dc55ca5130671c66f0
Hosein Khabbazian <hosenkh@gmail.com>
2015-08-07 23:52:34 +0430

    discard over-modularizing process!

--- file/src/tableController.js
+++
@@ -1,9 +0,0 @@
-/**
-   * the controller for the main page
-   * @param  {angular injection} $scope the scope
-   */
-  tableController = function($scope){
-    _.extend($scope, {
-      tableData: tableData
-    });
-  },
